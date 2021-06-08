7a1a59cbd39f326dc6562ba84efcb789b4bc4ba4
Ricky Arcenal Brain <brainrickyboy3@gmail.com>
2021-06-08 12:49:37 +0800

    Create OurPicks.vue

---
+++ src/components/OurPicks.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="hello">
+    <div class="menu">
+      <div class="menu-item">
+        <div class="menu-text">
+          Rate
+        </div>
+      </div>
+      <div class="menu-item">
+        <div class="menu-text">
+          Metacritic Scores
+        </div>
+      </div>
+      <div class="menu-item menu-center">
+        Video Game Rating System
+      </div>
+      <div class="menu-item">
+        <div class="menu-text">
+          YouTube Gameplays
+        </div>
+      </div>
+      <div class="menu-item">
+        <div class="menu-text">
+          Our Picks
+        </div>
+      </div>
+    </div>
+  
+
+    <div class="name">
+      <div class="letter">
+        <img src="../assets/img/crash.png" class="letter-image">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/drake.png" class="letter-image">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/gta.jpg" class="letter-image">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/pacman.jpg" class="letter-image">
+      </div>
+      <div class="letter">
+        <img src="../assets/img/lol.jpg" class="letter-image">
+      </div>
+      
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.name {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  width: 100vw;
+  height: calc(100vh - 60px);
+}
+.letter {
+  margin: .8%;
+  filter: drop-shadow(0px 0px 7px rgba(150, 150, 150, .7));
+}
+.letter-image {
+  width: 100%;
+}
+
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  height: 60px;
+}
+
+.menu-item {
+  flex: 1;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.menu-text{
+  font-size: 20px;
+  color: #818181;
+  cursor: pointer;
+  padding: 10px;
+  border-bottom: 2px solid #080808;
+  width: fit-content;
+}
+
+.menu-text:hover {
+  border-bottom: 2px solid #c278ff;
+}
+
+.menu-center {
+  font-size: 36px;
+  color: #EAEAEA;
+}
+</style>
